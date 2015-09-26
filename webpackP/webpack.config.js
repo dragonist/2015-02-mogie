a4ee13288f08066d46c70f50ebb53b66c7320135
dragonist <erin314@naver.com>
2015-09-26 22:02:24 +0900

    [web pack] Practice

---
+++ webpackP/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports={
+	entry: './index.jsx',
+	output: {
+		filename:'bundle.js',
+		publicPath:'http://loacalhost:8090/assets'
+	},
+	module:{
+		loaders:[{
+			test:/\.jsx$/,
+			loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+		}]
+	},
+	externals:{
+		'react':'React'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	}
+}
