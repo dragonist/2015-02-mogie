ddd3b6087054cfc00057da7e428f35bc550541eb
dragonist <erin314@naver.com>
2015-09-29 11:38:47 +0900

    [REACT] gif image 삭제 jsx 완료

--- real/components/index.jsx
+++ real/components/index.jsx
@@ -1,11 +1,11 @@
-
+var data = require('./movie3.js');
 
 var MovieBox = React.createClass({
   render: function() {
     return (
       	<li key={this.props.id} className="movie-box" data-key={this.props.id}>
       		<div className="box-image">
-      			<img src={this.props.src} alt={this.props.title}/>
+      			<img src={this.props.src} alt={this.props.title} width="185px"/>
       		</div>
       		<div className="box-contents">
       			<div className="contents">
@@ -41,24 +41,11 @@ var BoxContainer = React.createClass({
   }
 });
 
-var data = [
-  {"id":"77959","title":"빌리 엘리어트 뮤지컬 라이브","src":"img/img77959.jpg"},
-  {"id":"77992","title":"GROW : 인피니트의 리얼 청춘 라이프","src":"img/img77992.jpg"},
-  {"id":"76755","title":"7번방의 선물","src":"img/img76755.jpg"},
-  {"id":"38373","title":"(IMAX DMR 2D)다크 나이트","src":"img/img38373.gif"},
-  {"id":"38210","title":"추격자","src":"img/img38210.gif"},
-  {"id":"76625","title":"인피니트 콘서트 세컨드 인베이전 에볼루션 더 무비 3D","src":"img/img76625.jpg"},
-  {"id":"39679","title":"세 얼간이","src":"img/img39679.jpg"},
-  {"id":"77968","title":"제자, 옥한흠","src":"img/img77968.jpg"},
-  {"id":"76165","title":"언터처블 : 1%의 우정","src":"img/img76165.jpg"},
-  {"id":"38877","title":"아바타: 스페셜 에디션","src":"img/img38877.gif"}
-];
-
-
 // React.render(
 //   <CommentBox url="./../mockData/movie1.json" />,
 //   document.getElementById('Container')
 // );
+
 var Header = React.createClass({
   render:function () {
     return (
@@ -127,7 +114,6 @@ var ResultWrapper = React.createClass({
 
 var Body = React.createClass({
   render: function() {
-    
     return (
       <div>
         <Header/>
